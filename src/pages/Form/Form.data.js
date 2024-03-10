dd9e356a3183e640fbb77cbd4d332042959d02c6
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-03-10 21:39:42 +0100

    added the errorMessage to each input object

--- src/pages/Form/Form.data.js
+++ src/pages/Form/Form.data.js
@@ -1,31 +1,26 @@
-export const inputsArr = ({ values }) => {
+export const inputsArr = ({ values, errors, submitCount }) => {
   const inputs = [
     {
       id: 1,
       name: "username",
       type: "text",
       placeholder: "Username",
-      errorMessage: values?.errors?.username,
-      //   "Username should be 3-16 characters with no special characters.",
+      errorMessage: submitCount > 0 && errors?.username,
       label: "Username",
-      // pattern: "^[A-Za-z0-9]{3,16}$",
     },
     {
       id: 2,
       name: "email",
       type: "email",
       placeholder: "user@gmail.com",
-      errorMessage: values?.errors?.email,
-      // "Enter a valid email address.",
+      errorMessage: submitCount > 0 && errors?.email,
       label: "Email",
-      // pattern: "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$",
     },
     {
       id: 3,
       name: "birthday",
       type: "date",
       placeholder: "Birthday",
-      errorMessage: "",
       label: "Birthday",
     },
     {
@@ -33,22 +28,16 @@ export const inputsArr = ({ values }) => {
       name: "password",
       type: "password",
       placeholder: "Password",
-      errorMessage: values?.errors?.password,
-      // errorMessage:
-      //   "Password should be 8-20 characters and include at least a number, letter, and special character.",
+      errorMessage: submitCount > 0 && errors?.password,
       label: "Password",
-      // pattern:
-      //    "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$",
     },
     {
       id: 5,
       name: "confirmPassword",
       type: "password",
       placeholder: "Confirm Password",
-      errorMessage: values?.errors?.confirmPassword,
-      // errorMessage: "Passwords don't match",
+      errorMessage: submitCount > 0 && errors?.confirmPassword,
       label: "Confirm Password",
-      // pattern: values.password,
     },
   ];
 
